df52a6e9f9647208c379bcd65eef6ca2c7829a7d
Anber Alaa <anber.alaa27@gmail.com>
2025-12-27 16:20:02 +0200

    editLink

--- src/Components/DownloadBtn/DownloadBtn.jsx
+++ src/Components/DownloadBtn/DownloadBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function DownloadBtn() {
   return (
-    <a href="https://drive.google.com/file/d/1sb4UKqM6H_Q40XQMGRe7oYKg0hPMXTjA/view?usp=drive_link" target='blank' download="AnberAlaaCV.pdf" className="relative group cursor-pointer w-full max-w-[250px] mx-auto">
+    <a href="https://drive.google.com/file/d/1-itNVPMIcjv5nKw5EKJaxquH6F2rB6XN/view?usp=drive_link" target='blank' download="AnberAlaaCV.pdf" className="relative group cursor-pointer w-full max-w-[250px] mx-auto">
       <div className="relative text-center px-4 py-3 border-2 border-secondaryColor text-secondaryColor font-bold text-base md:text-lg rounded-lg transform transition-all duration-300 group-hover:translate-y-1 group-hover:translate-x-1 shadow-[6px_6px_10px_rgba(0,0,0,0.6),-6px_-6px_10px_rgba(255,255,255,0.1)] group-hover:shadow-[8px_8px_15px_rgba(0,0,0,0.8),-8px_-8px_15px_rgba(255,255,255,0.15)]">
         Download my CV
       </div>
